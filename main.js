fa0a5b6c89d68b33e13c1b99209e56c1570e0796
Michael Costello <mcostello91@gmail.com>
2022-06-10 16:04:46 -0700

    Clean up main.js and style.css

--- main.js
+++ main.js
@@ -8,10 +8,17 @@ $(function() {
 
     let [content] = contentArray.filter(el => el.length > 0);
 
+    let chips = $('#chips-wrapper');
+
     let contentParent = content.parent();
 
     content.hide();
+    chips.hide();
 
-    contentParent.prepend('<div class="container"><div id="motivational-text">Get back to following your dreams!</div><img src="https://cdn.pixabay.com/photo/2019/06/21/19/27/switzerland-4290226_960_720.jpg"></img><a href="https://www.pexels.com/photo/silhouette-of-mountains-1323550/">Photo by Simon Berger:</a></div>')
 
+    contentParent.addClass('container');
+    contentParent.prepend(
+        '<div id="motivational-text">Get back to following your dreams!</div>',
+        '<img src="https://cdn.pixabay.com/photo/2019/06/21/19/27/switzerland-4290226_960_720.jpg"></img>',
+        '<a href="https://www.pexels.com/photo/silhouette-of-mountains-1323550/">Photo by Simon Berger:</a>')
 })
