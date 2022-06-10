508de040991eed9f9e1245c7780a2e3c2975ff57
Michael Costello <mcostello91@gmail.com>
2022-06-10 13:36:19 -0700

    Refactor main.js using jQuery

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-let contentArray = [
-    document.getElementById('contents'),//youtube
-    document.getElementById('AppRouter-main-content'),//reddit
-    document.getElementById('posts') //tumblr
-    ];
+// let contentArray = [
+//     document.getElementById('contents'),//youtube
+//     document.getElementById('AppRouter-main-content'),//reddit
+//     document.getElementById('posts') //tumblr
+//     ];
 
-const [contents] = contentArray.filter(el => el !== null);
+// const [contents] = contentArray.filter(el => el !== null);
 
-contents.parentNode.removeChild(contents);
+// contents.parentNode.removeChild(contents);
+
+// $(function() {
+
+    let contentArray = [
+        $('#posts'), //tumblr
+        $('#contents'), //youtube
+        $('#AppRouter-main-content') //reddit
+    ];  
+
+    let [contents] = contentArray.filter(el => el.length > 0);
+
+    contents.hide();
+
+// })
