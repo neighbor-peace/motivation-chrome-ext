c594ef09c84905864d961cdd009ecb02913f3180
Michael Costello <mcostello91@gmail.com>
2022-06-10 15:54:58 -0700

    Add motivational image and text

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-// let contentArray = [
-//     document.getElementById('contents'),//youtube
-//     document.getElementById('AppRouter-main-content'),//reddit
-//     document.getElementById('posts') //tumblr
-//     ];
-
-// const [contents] = contentArray.filter(el => el !== null);
-
-// contents.parentNode.removeChild(contents);
-
-// $(function() {
+$(function() {
 
     let contentArray = [
         $('#posts'), //tumblr
@@ -16,8 +6,12 @@
         $('#AppRouter-main-content') //reddit
     ];  
 
-    let [contents] = contentArray.filter(el => el.length > 0);
+    let [content] = contentArray.filter(el => el.length > 0);
 
-    contents.hide();
+    let contentParent = content.parent();
 
-// })
+    content.hide();
+
+    contentParent.prepend('<div class="container"><div id="motivational-text">Get back to following your dreams!</div><img src="https://cdn.pixabay.com/photo/2019/06/21/19/27/switzerland-4290226_960_720.jpg"></img><a href="https://www.pexels.com/photo/silhouette-of-mountains-1323550/">Photo by Simon Berger:</a></div>')
+
+})
